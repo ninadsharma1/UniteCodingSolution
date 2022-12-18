1fa8235ef20e4ec67f64fd410fb77a9d940a85a4
ninadsharma1 <ninad2103@gmail.com>
2022-12-18 23:44:53 +0100

    Removed unnecessary prints

--- src/main/java/com/unite/solution/Solution.java
+++ src/main/java/com/unite/solution/Solution.java
@@ -16,28 +16,18 @@ public class Solution {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-
-        System.out.println("Given a range of numbered days (i...j),  and a number k, " +
-                "determine the number of days in the range that are beautiful. " +
-                "Beautiful numbers are defined as numbers where |i - reverse(i)| is evenly divisible by k. " +
-                "If a days value is a beautiful number, it is a beautiful day. " +
-                "Return the number of beautiful days in the range");
         try {
             Scanner stdin = new Scanner(System.in);
-
             // Taking input : Start range of number.
             int startDay = stdin.nextInt();
-
             // Taking input : End range of number.
             int endDay = stdin.nextInt();
-
             //Taking input divisor : Need to check if the number is evenly divided by divisor.
             int divisor = stdin.nextInt();
-
             //Creating the object for Beautiful days class and passing the arguments.
             BeautifulDays beautifulDays = new BeautifulDays();
             int result = beautifulDays.countBeautifulDaysInRange(startDay, endDay, divisor);
-            System.out.println("Count of beautiful days : " + result);
+            System.out.println(result);
         } catch (Exception exception) {
             System.out.println("Invalid input : " + exception.getMessage());
         }
